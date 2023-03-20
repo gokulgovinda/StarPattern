da9d22e7db13c45e8299dd3a40ed116a13857111
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2023-03-20 21:52:55 +0530

    Update Star.java

--- Star.java
+++ Star.java
@@ -33,3 +33,17 @@ int n=7
 	**
 	*
 		
+  	// Half pyramid
+		1
+		12
+		123
+		1234
+	
+	int n=4
+ for(int line=1; line<=n; line++) {
+		  for(int number=1;number<=line;number++) {
+		  System.out.print(number);
+	  }
+	 System.out.println();
+	 
+	 }
