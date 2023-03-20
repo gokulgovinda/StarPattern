682c23c60cf108fb29a86a90ddfcf69f5b461238
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2023-03-20 21:44:24 +0530

    Update Star.java

--- Star.java
+++ Star.java
@@ -1,17 +1,35 @@
 public class Stars {
 	 public static void main(String args[]) {
-	  for(int line=1; line<=4; line++) {
-		  for(int star=1;star<=line;star++) {
-		  System.out.print("*");
-	  }
-	 System.out.println();
+// 	  for(int line=1; line<=4; line++) {
+// 		  for(int star=1;star<=line;star++) {
+// 		  System.out.print("*");
+// 	  }
+// 	 System.out.println();
 	 
-	 }
-	 }
+// 	 }
+// 	 }
 }
 // output 
 // *
 // **
 // ***
 // ****
-
+	
+int n=7
+ for(int line=1; line<=n; line++) {
+		  for(int star=1;star<=n-line+1;star++) {
+		  System.out.print("*");
+	  }
+	 System.out.println();
+	 
+	 }
+	
+// 	Output
+	*******
+	******
+	*****
+	****
+	***
+	**
+	*
+		
